21e9acb707e3e7ca176de4c03da34e1469203630
Scott Zang <scott.zangliangjian@gmail.com>
2024-05-31 01:41:59 +0000

    minor changes

--- neat/genome.py
+++ neat/genome.py
@@ -124,19 +124,16 @@ class DefaultGenome(object):
     A genome for generalized neural networks.
 
     Terminology
-        pin: Point at which the network is conceptually connected to the external world;
-             pins are either input or output.
+        pin: Point at which the network is conceptually connected to the external world; pins are either input or output.
         node: Analog of a physical neuron.
-        connection: Connection between a pin/node output and a node's input, or between a node's
-             output and a pin/node input.
+        connection: Connection between a pin/node output and a node's input, or between a node's output and a pin/node input.
         key: Identifier for an object, unique within the set of similar objects.
 
     Design assumptions and conventions.
-        1. Each output pin is connected only to the output of its own unique
-           neuron by an implicit connection with weight one. This connection
-           is permanently enabled.
-        2. The output pin's key is always the same as the key for its
-           associated neuron.
+        1. Each output pin is connected only to the output of its own unique neuron 
+           by an implicit connection with weight one. 
+           This connection is permanently enabled.
+        2. The output pin's key is always the same as the key for its associated neuron.
         3. Output neurons can be modified but not deleted.
         4. The input values are applied to the input pins unmodified.
     """
