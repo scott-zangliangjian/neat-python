1573e26956c762f9b44bf11405dc349b7bb3183f
Scott Zang <scott.zangliangjian@gmail.com>
2024-05-31 14:49:42 +0000

    minor changes

--- tests/test_reproduction.py
+++ tests/test_reproduction.py
@@ -6,21 +6,14 @@ from neat.reproduction import DefaultReproduction
 class TestSpawnComputation(unittest.TestCase):
     def test_spawn_adjust1(self):
         adjusted_fitness = [1.0, 0.0]
-        previous_sizes = [20, 20]
-        pop_size = 40
+        previous_sizes   = [20, 20]
+        pop_size         = 40
         min_species_size = 10
 
-        spawn = DefaultReproduction.compute_spawn(adjusted_fitness, previous_sizes, pop_size, min_species_size)
-        self.assertEqual(spawn, [27, 13])
-
-        spawn = DefaultReproduction.compute_spawn(adjusted_fitness, spawn, pop_size, min_species_size)
-        self.assertEqual(spawn, [30, 10])
-
-        spawn = DefaultReproduction.compute_spawn(adjusted_fitness, spawn, pop_size, min_species_size)
-        self.assertEqual(spawn, [31, 10])
-
-        spawn = DefaultReproduction.compute_spawn(adjusted_fitness, spawn, pop_size, min_species_size)
-        self.assertEqual(spawn, [31, 10])
+        spawn = DefaultReproduction.compute_spawn(adjusted_fitness, previous_sizes, pop_size, min_species_size); self.assertEqual(spawn, [27, 13])
+        spawn = DefaultReproduction.compute_spawn(adjusted_fitness,          spawn, pop_size, min_species_size); self.assertEqual(spawn, [30, 10])
+        spawn = DefaultReproduction.compute_spawn(adjusted_fitness,          spawn, pop_size, min_species_size); self.assertEqual(spawn, [31, 10])
+        spawn = DefaultReproduction.compute_spawn(adjusted_fitness,          spawn, pop_size, min_species_size); self.assertEqual(spawn, [31, 10])
 
     def test_spawn_adjust2(self):
         adjusted_fitness = [0.5, 0.5]
